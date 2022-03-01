0adf60f5cbcdd8a9c5876dd162060d78a500df5d
anthoniusadi <anthonyfix0@gmail.com>
2022-03-01 22:55:23 +0700

    add database dan readme

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ import pdfkit
 app = Flask(__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_PASSWORD']='masuk'
 app.config['MYSQL_DB']='rebecca_kasir'
 mysql = MySQL(app)
 
